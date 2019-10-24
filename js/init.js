6653033f7733dea567fd44695c2852f2d1ca8816
Djk59 <waldyno97@gmail.com>
2019-10-24 18:16:32 +0200

    Fix images and index
    Adding new images and fix some html, css and js

--- js/init.js
+++ js/init.js
@@ -3,6 +3,15 @@
   
       $('.sidenav').sidenav();
       $('.parallax').parallax();
+      $('.modal').modal();
+      $(".dropdown-trigger").dropdown();
   
     }); // end of document ready
   })(jQuery); // end of jQuery name space
+
+
+  $( document ).ready(function() {
+    $(".dropdown-trigger").dropdown();
+    $('.modal').modal();
+});
+  
