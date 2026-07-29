d248382ecc11184258b03e8b626d3a53ac253cfe
WN59 <waldyno97@gmail.com>
2026-07-29 22:43:39 +0200

    Modifications carousel projets

--- js/carousel.js
+++ js/carousel.js
@@ -16,14 +16,6 @@ const projects = [
     liveUrl: "https://passeport-securite.com/",
   },
   {
-    title: "La Fondation du Nord",
-    description: "Le site web de la Fondation du Nord",
-    image:
-      "https://lafondationdunord.org/wp-content/uploads/2021/10/AUTONOMIE-1-scaled-e1635341850133.jpg",
-    technologies: ["Wordpress", "PHP", "CSS", "JavaScript"],
-    liveUrl: "https://lafondationdunord.org/",
-  },
-  {
     title: "Omegah Pôle Gérontologie - E-learning",
     description: "Application e-learning du Pôle Gérontologie PGI",
     image: "images/pgi.png",
